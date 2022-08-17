9a4685d360779f9ff2968268bc817f52a64c1d41
Joseph <86898815+JosephValenciaC@users.noreply.github.com>
2022-08-16 20:41:22 -0500

    listoo

--- Alvergue/Alvergue/settings.py
+++ Alvergue/Alvergue/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'inicio',
     'registros.apps.RegistrosConfig',
+    'colorfield',
 ]
 
 MIDDLEWARE = [
